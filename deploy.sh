eb3cb4e5b9af59c2236f8a7f45b3f62a0a06f3fa
karayuu <karayan0614@gmail.com>
2021-12-07 10:27:26 +0900

    【開発環境】さらにさらにエラーを修正

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ ssh karayuu@118.27.6.30 "cd ~/dev; yarn install;
  cd docker_deploy && docker-compose up -d --build;
  docker-compose exec -T web composer install;
  cd ..; cp .env.example .env;
- sed -i -e 's/DB_PASSWORD=default/DB_PASSWORD='$1'' .env;
+ sed -i -e \"'s/DB_PASSWORD=default/DB_PASSWORD='$1''\" .env;
  cd docker_deploy && docker-compose exec -T web php artisan key:generate;
  docker-compose run web php artisan migrate;
  yarn dev"

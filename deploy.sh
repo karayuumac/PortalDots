23f90296978c7b4a6af036e65fb6e51f3f40340a
karayuu <karayan0614@gmail.com>
2021-12-07 10:07:25 +0900

    【開発環境】MySQLのルートパスワードを設定できるように

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,11 @@
-rsync -acvz --delete ./ karayuu@118.27.6.30:~/dev --exclude 'node_modules' --exclude 'vendor' --exclude '.git' --exclude '.husky' --exclude 'docker_dev/data' --exclude 'storage'
-ssh karayuu@118.27.6.30 "cd ~/dev; yarn install; yarn docker; cd docker_dev/ && docker-compose exec -T web composer install; cd ..; cp .env.example .env; cd docker_dev && docker-compose exec -T web php artisan key:generate; cd ..; yarn migrate; yarn dev"
+rsync -acvz --delete ./ karayuu@118.27.6.30:~/dev --exclude 'node_modules' --exclude 'vendor' --exclude '.git' --exclude '.husky' --exclude 'docker_dev/data' --exclude 'docker_deploy/data' --exclude 'storage'
+ssh karayuu@118.27.6.30 "cd ~/dev; yarn install;
+ export MYSQL_ROOT_PASSWORD=$1;
+ cd docker_deploy && docker-compose up -d --build;
+ docker-compose exec -T web composer install;
+ cd ..; cp .env.example .env;
+ sed -i -e 's/DB_PASSWORD=default/DB_PASSWORD=$1' .env;
+ cd docker_deploy && docker-compose exec -T web php artisan key:generate;
+ docker-compose run web php artisan migrate;
+ yarn dev"
+

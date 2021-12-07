1801b233f0408c60ae984f08847404c0bdd16e6a
karayuu <karayan0614@gmail.com>
2021-12-07 11:02:33 +0900

    【開発環境】パスワードは変えとこうね〜

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ ssh karayuu@118.27.6.30 "cd ~/dev; yarn install;
  docker-compose exec -T web composer install;
  cd ..; cp .env.example .env;
  sed -i -e /^DB_HOST=/c\DB_HOST=portal_db_deploy .env;
+ sed -i -e /^DB_PASSWORD=/c\DB_PASSWORD=$1 .env;
  cd docker_deploy && docker-compose exec -T web php artisan key:generate;
  docker-compose run web php artisan config:cache;
  docker-compose run web php artisan migrate;

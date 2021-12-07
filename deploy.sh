c68d0119cf073674a7076b03b44bb73ba9022148
karayuu <karayan0614@gmail.com>
2021-12-07 10:48:08 +0900

    【開発環境】行けそう

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ ssh karayuu@118.27.6.30 "cd ~/dev; yarn install;
  cd docker_deploy && docker-compose up -d --build;
  docker-compose exec -T web composer install;
  cd ..; cp .env.example .env;
+ sed -i -e /^DB_HOST=/c\DB_HOST=portal_db_deploy .env;
  sed -i -e /^DB_PASSWORD=/c\DB_PASSWORD=$1 .env;
  cd docker_deploy && docker-compose exec -T web php artisan key:generate;
  docker-compose run web php artisan config:cache;
